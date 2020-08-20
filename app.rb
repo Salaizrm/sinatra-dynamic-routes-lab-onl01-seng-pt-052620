afecd545859ce3d5acbab5e5dedf1b439cc2f6cb
Robert Salaiz <work.salaizrm@gmail.com>
2020-08-20 00:05:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name' do
+    erb :reverse_name
+  end
+
+  get '/square/:number' do
+    erb :square_number
+  end
+
+  get '/say/:number/:phrase' do
+    erb :say_number_phrase
+  end
 
 end
